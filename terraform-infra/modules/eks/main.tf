7c485229d82c5da2339b70bbfb240d6ef256ffb1
devendraappa <devendraappa.kune@gmail.com>
2026-05-11 14:57:04 +0530

    added root access to view the nodes in aws console

--- terraform-infra/modules/eks/main.tf
+++ terraform-infra/modules/eks/main.tf
@@ -280,7 +280,7 @@ resource "aws_eks_access_policy_association" "root_admin" {
 
 #   access_config {
 #     authentication_mode                         = "API_AND_CONFIG_MAP"
-#     bootstrap_cluster_creator_admin_permissions = true
+#     bootstrap_cluster_creator_admin_permissions = false
 #   }
 
 #   tags = {
